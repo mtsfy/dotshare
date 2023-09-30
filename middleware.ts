3e2f006497318cdbb3a8b69e703ed556858faccb
Muse Tesfaye <tesfaye.musee@gmail.com>
2023-09-30 10:52:28 -0400

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,14 @@ export {default} from "next-auth/middleware";
 
 export const config = {
     matcher: [
+        "/:userId/followers",
+        "/:userId/following",
+        "/:userId/followers",
         "/edit/profile",
         "/friends",
         "/likes",
-        "/post",
+        "/post/:postId",
+        "/post/:postId/edit",
         "/saved",
     ]
 }
